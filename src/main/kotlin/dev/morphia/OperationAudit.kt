ff2c1edb11cbcdabc69476b77a0c7c15716442d5
evanchooly <jlee@antwerkz.com>
2019-11-22 11:21:37 -0500

    refinements

--- src/main/kotlin/dev/morphia/OperationAudit.kt
+++ src/main/kotlin/dev/morphia/OperationAudit.kt
@@ -2,7 +2,7 @@ package dev.morphia
 
 import org.asciidoctor.Asciidoctor.Factory
 import org.jsoup.Jsoup
-import java.io.FileWriter
+import java.io.File
 import java.lang.String.format
 import java.net.URL
 import java.util.*
@@ -17,13 +17,12 @@ class OperationAudit {
             .select(cssSelector)
             .distinctBy { it.text() }
             .map { it.text() }
-        if(operators.size == 0) {
+        if (operators.size == 0) {
             throw IllegalStateException("No operators found for $url.")
         }
         val properties = Properties()
         properties.load(this::class.java.getResourceAsStream(file))
         val property = properties.get(operators[0])
-        println("property = ${property}")
         var document = """= $url
 
 .${file}
@@ -42,18 +41,15 @@ class OperationAudit {
 
         document += "|==="
 
-        document.lines()
-            .forEachIndexed<String>  { index, line ->
-                println(format("%3d %s", index, line))
-            }
-        var writer = FileWriter("target/doc.adoc")
-        writer.write(document)
-        writer.flush()
-
-        val doc = asciidoctor.convert(document, mapOf())
-        writer = FileWriter("target/temp.html")
-        writer.write(doc)
-        writer.flush()
+        try {
+            File("target/temp.html").writeText(asciidoctor.convert(document, mapOf()))
+        } catch (e: Exception) {
+            document.lines()
+                .forEachIndexed<String> { index, line ->
+                    println(format("%3d %s", index, line))
+                }
+            File("target/doc.adoc").writeText(document)
+        }
 
         return this
     }
@@ -63,8 +59,10 @@ class OperationAudit {
 
 fun main(args: Array<String>) {
     OperationAudit()
-        .check("https://docs.mongodb.com/manual/reference/operator/query/", ".xref.mongodb-query",
-            "/query-operators.properties")
+        .check(
+            "https://docs.mongodb.com/manual/reference/operator/query/", ".xref.mongodb-query",
+            "/query-operators.properties"
+        )
 //        .check("https://docs.mongodb.com/manual/meta/aggregation-quick-reference", ".xref.mongodb-pipeline",
 //            "/query-operators.properties")
 }
