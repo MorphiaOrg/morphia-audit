a4938bf462087682c6202b4c12fbc10e8e684fe6
evanchooly <jlee@antwerkz.com>
2020-01-01 23:08:05 -0500

    key off the @mongodb.driver.manual taglet to auto document implementations

--- src/main/kotlin/dev/morphia/OperationAudit.kt
+++ src/main/kotlin/dev/morphia/OperationAudit.kt
@@ -1,7 +1,10 @@
 package dev.morphia
 
 import org.asciidoctor.Asciidoctor.Factory
-import org.asciidoctor.jruby.cli.AsciidoctorInvoker
+import org.jboss.forge.roaster.Roaster
+import org.jboss.forge.roaster.model.JavaType
+import org.jboss.forge.roaster.model.MethodHolder
+import org.jboss.forge.roaster.model.source.MethodSource
 import org.jsoup.Jsoup
 import java.io.File
 import java.io.FileInputStream
@@ -9,8 +12,28 @@ import java.net.URL
 import java.util.Properties
 
 
-class OperationAudit {
-    fun check(url: String, cssSelector: String, file: File): OperationAudit {
+class OperationAudit(var methods: Map<String, List<MethodSource<*>>>) {
+    companion object {
+        fun parse(vararg pkgNames: String): OperationAudit {
+            val file = File(System.getenv("MORPHIA_SRC"), "src/main/java").absoluteFile
+            println("Scanning $file for sources")
+            val methods = file.walkBottomUp()
+                .filter { it.extension == "java" }
+                .map { Roaster.parse(JavaType::class.java, it) }
+                .filter { it.getPackage() in pkgNames }
+                .filterIsInstance<MethodHolder<*>>()
+                .map { it.getMethods() }
+                .flatten()
+                .filterIsInstance<MethodSource<*>>()
+                .filter { it.getJavaDoc().tagNames.contains("@mongodb.driver.manual") }
+                .groupBy { it.getJavaDoc().getTags("@mongodb.driver.manual")[0].value.substringAfter(" ") }
+
+            return OperationAudit(methods)
+        }
+
+    }
+
+    fun audit(url: String, cssSelector: String, file: File) {
         val operators = Jsoup.parse(URL(url), 30000)
             .select(cssSelector)
             .distinctBy { it.text() }
@@ -19,8 +42,6 @@ class OperationAudit {
         if (operators.isEmpty()) {
             throw IllegalStateException("No operators found for $url.")
         }
-        val properties = Properties()
-        properties.load(FileInputStream(file))
         var document = """= $url
 
 .${file}
@@ -32,11 +53,11 @@ class OperationAudit {
             document += """
             |${operator}
             |""".trimIndent()
-            properties[operator]?.let {
-                it as String
-                for (s in it.split(";")) {
-                document += ". $s\n"
-                }
+            methods[operator]?.map { method ->
+                val name = method.getName()
+                val className = method.getOrigin().getQualifiedName()
+                method.removeJavaDoc()
+                document += ". ${method}\n"
             }
             document += "\n"
         }
@@ -47,21 +68,19 @@ class OperationAudit {
 
         File("target/${file.extension("html")}").writeText(asciidoctor.convert(document, mapOf()))
         File("target/${file.extension("adoc")}").writeText(document)
-
-        return this
     }
 
     private fun File.extension(extension: String) = this.nameWithoutExtension + ".$extension"
 }
 
 fun main() {
-    OperationAudit()
-        .check(
-            "https://docs.mongodb.com/manual/reference/operator/query/", ".xref.mongodb-query",
-            File("src/main/resources/query.properties")
-        )
-        .check(
-            "https://docs.mongodb.com/manual/meta/aggregation-quick-reference", ".xref.mongodb-pipeline",
-            File("src/main/resources/aggregation.properties")
-        )
+    OperationAudit
+        .parse(/*"dev.morphia.aggregation", */"dev.morphia.aggregation.experimental")
+        .audit("https://docs.mongodb.com/manual/meta/aggregation-quick-reference", ".xref.mongodb-pipeline",
+            File("src/main/resources/aggregation-pipeline.properties"))
+
+//        .audit("https://docs.mongodb.com/manual/reference/operator/query/", ".xref.mongodb-query",
+//            File("src/main/resources/query.properties"))
+//        .audit("https://docs.mongodb.com/manual/meta/aggregation-quick-reference", ".xref.mongodb-expression",
+//            File("src/main/resources/aggregation-expressions.properties"))
 }
