ee34861767b0eefac54928989a072952c92a6ddf
evanchooly <jlee@antwerkz.com>
2020-04-18 13:12:03 -0400

    new audits unified reporting

--- src/main/kotlin/dev/morphia/SinceAudit.kt
+++ src/main/kotlin/dev/morphia/SinceAudit.kt
@@ -1,67 +1,207 @@
 package dev.morphia
 
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.joinAll
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import org.eclipse.jgit.api.CreateBranchCommand
-import org.eclipse.jgit.api.Git
-import org.jboss.forge.roaster.Roaster
+import dev.morphia.model.MorphiaClass
+import dev.morphia.model.MorphiaMethod
+import dev.morphia.model.State.ABSENT
+import dev.morphia.model.State.DEPRECATED
+import dev.morphia.model.State.PRESENT
+import dev.morphia.model.Version
+import org.objectweb.asm.ClassReader
+import org.objectweb.asm.ClassReader.SKIP_CODE
+import org.objectweb.asm.Opcodes
+import org.objectweb.asm.Opcodes.ACC_BRIDGE
+import org.objectweb.asm.Opcodes.ACC_DEPRECATED
+import org.objectweb.asm.Opcodes.ACC_PROTECTED
+import org.objectweb.asm.Opcodes.ACC_PUBLIC
+import org.objectweb.asm.Opcodes.ACC_SYNTHETIC
+import org.objectweb.asm.tree.ClassNode
+import org.objectweb.asm.tree.MethodNode
 import java.io.File
-
-class SinceAudit() {
-    private var git: Git
-    val repoDir = File("target/morphia.audit")
-
-    init {
-        git = if (repoDir.exists()) {
-            Git.open(repoDir)
-        } else {
-            println("Cloning morphia repository to ${repoDir}")
-            Git.cloneRepository()
-                .setURI("git@github.com:MorphiaOrg/morphia.git")
-                .setDirectory(repoDir)
-                .call()
-        }
-
-        val branches = git.branchList().call()
-        if (branches.none { it.name == "refs/heads/2.0-dev" }) {
-            git.checkout()
-                .setName("2.0-dev")
-                .setStartPoint("origin/2.0-dev")
-                .setCreateBranch(true)
-                .setUpstreamMode(CreateBranchCommand.SetupUpstreamMode.SET_UPSTREAM)
-                .call()
-        }
-    }
-
-    fun run() {
-        baseLine()
-    }
-
-    private fun baseLine() {
-        println("looking for files")
-        val javaFiles = File(repoDir, "morphia/src/main/java").walkTopDown()
-            .filter { it.extension == "java" }
-            .toList()
-
-        for (javaFile in javaFiles) {
-            runBlocking {
-                val jobs = mutableListOf<Job>()
-                jobs += launch { catalog(javaFile) }
-
-                jobs.joinAll()
-            }
-        }
-
-    }
-
-    private fun catalog(javaFile: File) {
-        val parse = Roaster.parse(javaFile)
-        println("parse = ${parse.getQualifiedName()}")
-    }
-}
+import java.io.FileOutputStream
+import java.net.URL
+import java.util.ArrayList
+import java.util.LinkedHashMap
+import java.util.concurrent.ConcurrentHashMap
+import java.util.jar.JarFile
 
 fun main() {
     SinceAudit().run()
+}
+
+class SinceAudit() {
+    val classHistory = ConcurrentHashMap<String, MorphiaClass>()
+    val methodHistory = ConcurrentHashMap<String, MorphiaMethod>()
+    val classes = javaClass.getResourceAsStream("/audit-classes.txt")
+        .bufferedReader()
+        .readLines()
+        .filterNot { it.startsWith("#") }
+    val reports = mutableMapOf<String, () -> Unit>()
+
+    fun run() {
+        Version.values().forEach { version ->
+            version.download().also {
+                it.entries().iterator()
+                    .forEach { entry ->
+                        if (entry.name.endsWith("class") && !entry.name.endsWith("module-info.class")) {
+                            val classNode = ClassNode().also { node ->
+                                ClassReader(it.getInputStream(entry)).accept(node, SKIP_CODE)
+                            }
+
+                            if (classNode.className() in classes) {
+                                val morphiaClass = record(classNode)
+                                morphiaClass.versions[version] =
+                                    if (classNode.access.isDeprecated()) DEPRECATED else PRESENT
+
+                                classNode.methods.forEach { m ->
+                                    if (m.access.isNotPrivate() && m.access.isNotSynthetic()) {
+                                        val method = record(morphiaClass, m)
+                                        method.versions[version] = if (m.access.isDeprecated()) DEPRECATED else PRESENT
+                                    }
+                                }
+                            }
+                        }
+                    }
+            }
+        }
+
+        validate()
+
+        if(reports.isNotEmpty()) {
+            reports.values.forEach { it() }
+            throw IllegalStateException("Violations found")
+        }
+    }
+
+    private fun validate() {
+        missingNondeprecatedMethods(Version.v2_0_0_SNAPSHOT, Version.v1_6_0_SNAPSHOT)
+        newDeprecatedMethods(Version.v2_0_0_SNAPSHOT, Version.v1_6_0_SNAPSHOT)
+        newDeprecatedClasses(Version.v2_0_0_SNAPSHOT, Version.v1_6_0_SNAPSHOT)
+    }
+
+    private fun newDeprecatedMethods(newer: Version, older: Version) {
+        val list = methodHistory.values
+            .filter { m ->
+                m.versions[newer] == DEPRECATED && m.versions[older] == ABSENT
+            }
+            .sortedBy { it.name }
+
+        reportMethods("New deprecated methods in ${newer}", older, newer, list);
+    }
+
+    private fun newDeprecatedClasses(newer: Version, older: Version) {
+        val list = classHistory.values
+            .filter { c ->
+                c.versions[newer] == DEPRECATED && c.versions[older] == ABSENT
+            }
+            .sortedBy { it.name }
+        reportClasses("New deprecated classes in ${newer}", older, newer, list);
+    }
+
+    private fun missingNondeprecatedMethods(newer: Version, older: Version) {
+        val list = methodHistory.values
+            .filter { m ->
+                m.versions[newer] == ABSENT && m.versions[older] == PRESENT
+            }
+            .filter { classHistory["${it.pkgName}.${it.className}"]?.versions?.get(older) == PRESENT }
+            .sortedBy { it.name }
+
+        reportMethods("Methods missing in ${newer} that weren't deprecated in ${older}".format(newer, older),
+            older, newer, list
+        )
+    }
+
+    private fun reportMethods(title: String, older: Version, newer: Version, list: List<MorphiaMethod>) {
+
+        if (list.isNotEmpty()) reports[title] = {
+            println("${title}: ${list.size}")
+            val versions = "%-10s %-10s"
+            println(versions.format("older", "newer"))
+            list.forEach {
+                val states = versions.format(it.versions[older], it.versions[newer])
+                println("$states ${it.pkgName}.${it.className}#${it.name}")
+            }
+        }
+    }
+
+    private fun reportClasses(title: String, older: Version, newer: Version, list: List<MorphiaClass>) {
+        if (list.isNotEmpty()) reports[title] = {
+            println("${title}: ${list.size}")
+            val versions = "%-10s %-10s"
+            list.forEach {
+                val states = versions.format(it.versions[older], it.versions[newer])
+                println("$states ${it.fqcn()}")
+            }
+        }
+    }
+
+    private fun record(classNode: ClassNode): MorphiaClass {
+        return classHistory.computeIfAbsent("${classNode.packageName()}.${classNode.className()}") { _ ->
+            MorphiaClass(classNode.packageName(), classNode.className())
+        }
+    }
+
+    private fun record(parent: MorphiaClass, node: MethodNode): MorphiaMethod {
+        val descriptor = node.name + node.desc
+
+        return methodHistory.computeIfAbsent("${parent.fqcn()}#${descriptor}") { _ ->
+            MorphiaMethod(parent.pkgName, parent.name, descriptor)
+        }
+    }
+}
+
+fun URL.download(jar: File): JarFile {
+    if (!jar.exists()) {
+        FileOutputStream(jar).write(readBytes())
+    }
+    return JarFile(jar)
+}
+
+fun ClassNode.className(): String {
+    return this.name
+        .substringAfterLast("/")
+        .substringBeforeLast(".")
+        .replace("/", ".")
+}
+
+fun ClassNode.packageName(): String {
+    return this.name
+        .substringAfter("classes/")
+        .substringBeforeLast("/")
+        .replace("/", ".")
+}
+
+fun Int.isNotPrivate(): Boolean {
+    return isPublic() || isProtected()
+}
+
+fun Int.isNotSynthetic(): Boolean {
+    return !matches(ACC_BRIDGE) && !matches(ACC_SYNTHETIC)
+}
+
+fun Int.isDeprecated() = matches(ACC_DEPRECATED)
+
+fun Int.isProtected() = matches(ACC_PROTECTED)
+
+fun Int.isPublic() = matches(ACC_PUBLIC)
+
+fun Int.matches(mask: Int) = (this and mask) == mask
+
+fun Int.accDecode(): List<String>? {
+    val decode: MutableList<String> = ArrayList()
+    val values: MutableMap<String, Int> = LinkedHashMap()
+    try {
+        for (f in Opcodes::class.java.declaredFields) {
+            if (f.name.startsWith("ACC_")) {
+                values[f.name] = f.getInt(Opcodes::class.java)
+            }
+        }
+    } catch (e: IllegalAccessException) {
+        throw RuntimeException(e.message, e)
+    }
+    for ((key, value) in values) {
+        if (this.matches(value)) {
+            decode.add(key)
+        }
+    }
+    return decode
 }
---
+++ src/main/kotlin/dev/morphia/model/MorphiaElement.kt
@@ -0,0 +1,67 @@
+package dev.morphia.model
+
+import dev.morphia.download
+import java.io.File
+import java.net.URL
+import java.util.jar.JarFile
+
+class MorphiaClass(val pkgName: String, val name: String) {
+    var deprecated = false
+    var versions: MutableMap<Version, State> = Version.values().map { it to State.ABSENT }
+        .toMap().toMutableMap()
+
+    override fun toString(): String {
+        return "MorphiaClass(pkgName='$pkgName', name='$name')"
+    }
+
+    fun fqcn(): String {
+        return "$pkgName.$name"
+    }
+
+}
+
+class MorphiaMethod(val pkgName: String, val className: String, val name: String) {
+    var versions: MutableMap<Version, State> = Version.values()
+        .map { it to State.ABSENT }
+        .toMap()
+        .toMutableMap()
+
+
+    override fun toString(): String {
+        return "MorphiaMethod(name='${pkgName}.${className}#${name}', versions=$versions)"
+    }
+}
+
+enum class Version {
+    v2_0_0_SNAPSHOT {
+        override fun artifactId(): String = "morphia-core"
+        override fun version() = "2.0.0-SNAPSHOT"
+    },
+    v1_6_0_SNAPSHOT {
+        override fun version() = "1.6.0-SNAPSHOT"
+    },
+    v1_4_1 {
+        override fun version() = "1.4.1"
+    };
+
+    open fun artifactId() = "core"
+    fun groupId() = "dev.morphia.morphia"
+    abstract fun version(): String
+
+    fun download(): JarFile {
+        val groupId = groupId().replace('.', '/')
+
+        val path = "$groupId/${artifactId()}/${version()}/${artifactId()}-${version()}.jar"
+        val jar = File(System.getProperty("user.home"), ".m2/repository/$path")
+
+        return URL("${baseUrl()}$path").download(jar)
+    }
+
+    private fun baseUrl() = "https://repo1.maven.org/maven2/"
+}
+
+enum class State {
+    ABSENT,
+    PRESENT,
+    DEPRECATED
+}
